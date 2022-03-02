ce1ed459dda9870eb7a01e91f8e20d93845506f2
Vishnu <vishnuvichuz198@gmail.com>
2022-03-02 18:15:24 +0530

    razorpay added

--- api/models/User.js
+++ api/models/User.js
@@ -7,6 +7,7 @@ const UserSchema = new mongoose.Schema(
     password: { type: String, required: true },
     profilePic: { type: String, default: "" },
     isAdmin: { type: Boolean, default: false },
+    isPayed: {type:Boolean,default:false}
   },
   { timestamps: true }
 );
